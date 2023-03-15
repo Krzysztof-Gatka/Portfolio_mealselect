f31e343bd38f2331909c3762f3cd640a2e34754c
Krzysztof <krzysztof.gatka.webdev@gmail.com>
2023-03-15 21:44:58 +0100

    feat: reactive forms setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingListElementComponent } from './shopping-list/shopping-list-element/shopping-list-element.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { ShoppingListElementComponent } from './shopping-list/shopping-list-elem
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
