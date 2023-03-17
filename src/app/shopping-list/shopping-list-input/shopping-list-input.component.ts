c2aed97ec61b7f70dab61fd6333036586245ce53
Krzysztof <krzysztof.gatka.webdev@gmail.com>
2023-03-17 19:14:31 +0100

    feat: created input component

--- src/app/shopping-list/shopping-list-input/shopping-list-input.component.ts
+++ src/app/shopping-list/shopping-list-input/shopping-list-input.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
+import { Product } from '../shopping-list-element/product.model';
 
 @Component({
   selector: 'app-shopping-list-input',
@@ -6,5 +9,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./shopping-list-input.component.scss']
 })
 export class ShoppingListInputComponent {
+  form = new FormGroup({
+    productName: new FormControl('', Validators.required),
+    productQuantity: new FormControl('', Validators.pattern(/^[1-9][0-9]*/)),
+    productUnit: new FormControl('',)
+  })
 
+  @Output() itemAdded = new EventEmitter<Product>();
+
+  onAddButtonClick(): void {
+    const name = this.form.controls.productName.value!;
+    const quantity = +this.form.controls.productQuantity.value!;
+    const unit = this.form.controls.productUnit.value!;
+
+    const newProduct = new Product(name, quantity, unit);
+    this.itemAdded.emit(newProduct);
+    this.form.reset();
+  }
+
+  onClearButtonClick(): void {
+    this.form.reset();
+  }
 }
