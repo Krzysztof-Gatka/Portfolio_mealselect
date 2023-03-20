a6827ad874d48b45b582d442d9cdae9b6fd988b5
Krzysztof <krzysztof.gatka.webdev@gmail.com>
2023-03-20 22:37:13 +0100

    feat: template and styles for auth Comp

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
   styleUrls: ['./auth.component.scss']
 })
-export class AuthComponent {
+export class AuthComponent implements OnInit, OnDestroy {
+  type: string = '';
+  sub: Subscription | undefined;
+  constructor(private route: ActivatedRoute) {}
 
+  ngOnInit(): void {
+    this.sub = this.route.url.subscribe((url) => {
+      this.type = url[0].path;
+    })
+  }
+
+  ngOnDestroy(): void {
+    this.sub?.unsubscribe();
+  }
 }
