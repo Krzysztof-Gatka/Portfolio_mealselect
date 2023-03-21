30e05181a2f8b3436dec61e7a08558b6cbe482b1
Krzysztof <krzysztof.gatka.webdev@gmail.com>
2023-03-21 10:09:42 +0100

    feat: firebase authentication

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -22,7 +23,8 @@ import { AuthComponent } from './auth/auth.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,7 +1,12 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { Subscription } from 'rxjs';
+
+const WebAPIKey = 'AIzaSyA-qhl-Xy0a_-sxbwrPPeU_dLycYdAOTBo';
+const SignInUrl = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' + WebAPIKey;
+const LogInUrl = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=' + WebAPIKey;
 
 @Component({
   selector: 'app-auth',
@@ -10,13 +15,13 @@ import { Subscription } from 'rxjs';
 })
 export class AuthComponent implements OnInit, OnDestroy {
   form = new FormGroup({
-    login: new FormControl('', Validators.required),
+    email: new FormControl('', Validators.required),
     password: new FormControl('', Validators.required),
   })
 
   type: string = '';
   sub: Subscription | undefined;
-  constructor(private route: ActivatedRoute) {}
+  constructor(private route: ActivatedRoute, private http: HttpClient) {}
 
   ngOnInit(): void {
     this.sub = this.route.url.subscribe((url) => {
@@ -29,6 +34,24 @@ export class AuthComponent implements OnInit, OnDestroy {
   }
 
   onSubmit(): void {
+    const url = this.type === 'login' ? LogInUrl : SignInUrl
+    const userEmail = this.form.controls.email.value;
+    const userPassword = this.form.controls.password.value;
+
+    this.http.post<Response>(url, {
+      email: userEmail,
+      password: userPassword,
+      returnSecureToken: true,
+    })
+    .subscribe((response) => {
+      console.log(response);
+    })
+
     this.form.reset();
+  }
+
+  onTestUserClick(): void {
+    this.form.controls.email.setValue('test@test.com');
+    this.form.controls.password.setValue('12341234');
   }
 }
