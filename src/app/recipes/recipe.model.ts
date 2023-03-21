f423b3307c61874eebc5e46573237a0560b1b269
Krzysztof <krzysztof.gatka.webdev@gmail.com>
2023-03-21 14:30:11 +0100

    feat: add recipe model v1.0

---
+++ src/app/recipes/recipe.model.ts
@@ -0,0 +1,17 @@
+import { Product } from "../shopping-list/shopping-list-element/product.model";
+
+export class Recipe {
+  constructor(
+    public name: string,
+    public ingredients: Product[],
+    public prepSteps: string[],
+
+  ) {}
+
+  public prepTime: string | undefined;
+  public difficulty: string | undefined;
+  public tags: string[] | undefined;
+  public lastPrepDate: Date | undefined;
+
+
+}
