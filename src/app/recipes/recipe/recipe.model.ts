1844c99f70662e886e7ed64bfc952d58cd15eb41
Krzysztof <krzysztof.gatka.webdev@gmail.com>
2023-03-22 12:09:41 +0100

    refactor: test data update

--- src/app/recipes/recipe/recipe.model.ts
+++ src/app/recipes/recipe/recipe.model.ts
@@ -5,13 +5,12 @@ export class Recipe {
     public name: string,
     public ingredients: Product[],
     public prepSteps: string[],
-
+    public prepTime: string = '',
+    public difficulty: string = '',
+    public tags: string[] = [],
+    public lastPrepDate: Date | number = -1,
   ) {}
 
-  public prepTime: string | undefined;
-  public difficulty: string | undefined;
-  public tags: string[] | undefined;
-  public lastPrepDate: Date | undefined;
 
 
 }
@@ -38,7 +37,10 @@ export const testRecipes = [
       'mix pasta with eggs and parmezan mixture',
       'add bacon',
       'serve with fresh grated cheese and pepper'
-    ]
+    ],
+    '30 min',
+    'easy',
+    ['italian', 'pasta', 'quick', ],
   ),
   new Recipe(
     'Spaghetii Bolognese',
@@ -59,6 +61,102 @@ export const testRecipes = [
       'mix meat with tomatos on the pan',
       'boil pasta according to package instructions',
       'serve with fresh grated cheese and pepper'
-    ]
+    ],
+    '45 min',
+    'easy',
+  ),
+  new Recipe(
+    'Spaghetii Carbonara',
+    [
+      new Product('pasta', 500, 'g'),
+      new Product('egg', 4, 'pcs'),
+      new Product('bacon', 200, 'g'),
+      new Product('Parmezan', 200, 'g'),
+      new Product('salt', 1, 'g'),
+      new Product('pepper', 1, 'g')
+    ],
+    [
+      'boil water',
+      'crush Parmezan',
+      'chop bacon',
+      'mix eggs with parmezan and pepper',
+      'fry bacon till golden',
+      'boil pasta according to package instructions',
+      'mix pasta with eggs and parmezan mixture',
+      'add bacon',
+      'serve with fresh grated cheese and pepper'
+    ],
+    '30 min',
+    'easy',
+  ),
+  new Recipe(
+    'Spaghetii Bolognese',
+    [
+      new Product('pasta', 500, 'g'),
+      new Product('minced beef', 300, 'g'),
+      new Product('tomato can', 2, 'pc'),
+      new Product('Parmezan', 50, 'g'),
+      new Product('red wine', 1, 'bottle'),
+      new Product('salt', 1, 'g'),
+      new Product('pepper', 1, 'g')
+    ],
+    [
+      'boil water',
+      'crush Parmezan',
+      'chop bacon',
+      'fry meat with red wine',
+      'mix meat with tomatos on the pan',
+      'boil pasta according to package instructions',
+      'serve with fresh grated cheese and pepper'
+    ],
+    '45 min',
+    'easy',
+  ),
+  new Recipe(
+    'Spaghetii Carbonara',
+    [
+      new Product('pasta', 500, 'g'),
+      new Product('egg', 4, 'pcs'),
+      new Product('bacon', 200, 'g'),
+      new Product('Parmezan', 200, 'g'),
+      new Product('salt', 1, 'g'),
+      new Product('pepper', 1, 'g')
+    ],
+    [
+      'boil water',
+      'crush Parmezan',
+      'chop bacon',
+      'mix eggs with parmezan and pepper',
+      'fry bacon till golden',
+      'boil pasta according to package instructions',
+      'mix pasta with eggs and parmezan mixture',
+      'add bacon',
+      'serve with fresh grated cheese and pepper'
+    ],
+    '30 min',
+    'easy',
+  ),
+  new Recipe(
+    'Spaghetii Bolognese',
+    [
+      new Product('pasta', 500, 'g'),
+      new Product('minced beef', 300, 'g'),
+      new Product('tomato can', 2, 'pc'),
+      new Product('Parmezan', 50, 'g'),
+      new Product('red wine', 1, 'bottle'),
+      new Product('salt', 1, 'g'),
+      new Product('pepper', 1, 'g')
+    ],
+    [
+      'boil water',
+      'crush Parmezan',
+      'chop bacon',
+      'fry meat with red wine',
+      'mix meat with tomatos on the pan',
+      'boil pasta according to package instructions',
+      'serve with fresh grated cheese and pepper'
+    ],
+    '45 min',
+    'easy',
   ),
 ]
