00bd775b658f9db9c270d3add1e97dc26ad1e4d5
Krzysztof <krzysztof.gatka.webdev@gmail.com>
2023-03-22 12:58:54 +0100

    feat: add recipesService

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
-import { Recipe, testRecipes } from '../recipe/recipe.model';
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe/recipe.model';
+import { RecipesService } from '../recipes.service';
 
 @Component({
   selector: 'app-recipes-list',
   templateUrl: './recipes-list.component.html',
   styleUrls: ['./recipes-list.component.scss']
 })
-export class RecipesListComponent {
-  recipes: Recipe[] = testRecipes;
+export class RecipesListComponent implements OnInit{
+  recipes: Recipe[] | undefined;
+
+  constructor(private recipesService: RecipesService) {}
+
+  ngOnInit(): void {
+    this.recipes = this.recipesService.testRecipes
+  }
 }
