9aa21fb6bcb4edb3010a4e28f3844600b9adb8fc
Krzysztof Gatka <krzysztof.gatka.webdev@gmail.com>
2023-03-27 14:43:50 +0200

    feat: add routing guard

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,14 +1,30 @@
 import { HttpClient } from "@angular/common/http";
-import { Injectable } from "@angular/core";
-import { Router } from "@angular/router";
+import { inject, Injectable } from "@angular/core";
+import { ActivatedRoute, ActivatedRouteSnapshot, CanActivateFn, Router, RouterStateSnapshot, UrlTree } from "@angular/router";
+import { User } from "./user.model";
 
 
 const WebAPIKey = 'AIzaSyA-qhl-Xy0a_-sxbwrPPeU_dLycYdAOTBo';
 const SignInUrl = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' + WebAPIKey;
 const LogInUrl = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=' + WebAPIKey;
 
+class LoginResponse {
+  constructor(
+    public displayName: string,
+    public email: string,
+    public expiresIn: string,
+    public idToken: string,
+    public kind: string,
+    public localId: string,
+    public refreshToken: string,
+    public registered: boolean
+  ) {}
+
+}
+
 @Injectable({providedIn: 'root'})
 export class AuthService {
+  user: User | undefined;
 
   constructor(private http: HttpClient, private router: Router) {}
 
@@ -23,9 +39,14 @@ export class AuthService {
 
   logIn(userEmail: string, userPassword: string): void {
     const requestBody = this._createRequestBody(userEmail, userPassword);
-    this.http.post<Response>(LogInUrl, requestBody)
+    this.http.post<LoginResponse>(LogInUrl, requestBody)
       .subscribe((response) => {
-        console.log(response);
+        const email = response.email;
+        const expiresIn = parseInt(response.expiresIn);
+        const lastLogin = new Date();
+        const token = response.idToken;
+        this.user = new User(email, lastLogin, token, expiresIn);
+        console.log(this.user);
         this.router.navigate(['/home']);
       });
   }
@@ -38,4 +59,16 @@ export class AuthService {
     }
   }
 
+  canActivate(): boolean | UrlTree {
+    console.log('canActivate launched!');
+    const tree: UrlTree = this.router.parseUrl('/welcome');
+    if (this.user) return true;
+    return tree;
+  }
+
 }
+
+export const canActivateUser: CanActivateFn =
+  (route: ActivatedRouteSnapshot, state: RouterStateSnapshot) => {
+    return inject(AuthService).canActivate();
+  };
