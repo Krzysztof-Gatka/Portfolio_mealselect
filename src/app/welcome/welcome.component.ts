b6b29e8446a7f2558b50daa969937b76dc1b05fe
Krzysztof Gatka <krzysztof.gatka.webdev@gmail.com>
2023-03-28 14:12:06 +0200

    feat: add auth buttons visibilty toggle

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth/auth.service';
 
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.component.html',
   styleUrls: ['./welcome.component.scss']
 })
-export class WelcomeComponent {
+export class WelcomeComponent implements OnInit{
+  userLoggedIn: boolean = false;
+  constructor(private authService: AuthService) {}
 
+  ngOnInit(): void {
+    this.userLoggedIn = this.authService.isUserLoggedIn();
+    this.authService.userAuthentication.subscribe((operation) => {
+      if(operation === 'logIn' || operation === 'signIn') {
+        this.userLoggedIn = true;
+      }
+
+      if(operation === 'logOut') {
+        this.userLoggedIn = false;
+      }
+    })
+  }
 }
