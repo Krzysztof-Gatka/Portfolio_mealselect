12122d152cfd9ac9094a41c48c0bc17fe2944748
Krzysztof Gatka <krzysztof.gatka.webdev@gmail.com>
2023-03-31 13:43:37 +0200

    feat: add pantry elements preview

--- src/app/pantry/pantry.component.ts
+++ src/app/pantry/pantry.component.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Product } from '../shopping-list/shopping-list-element/product.model';
+import { PantryService } from './pantry.service';
 
 @Component({
   selector: 'app-pantry',
   templateUrl: './pantry.component.html',
   styleUrls: ['./pantry.component.scss']
 })
-export class PantryComponent {
+export class PantryComponent implements OnInit{
   pantry: Product[] = [];
+
+  constructor(private pantryServcie: PantryService) {}
+
+  ngOnInit(): void {
+    this.pantry = this.pantryServcie.getPantry();
+  }
 }
+
