009ef2dde08fb075a24cb272d07f656ed5fe132c
Krzysztof Gatka <krzysztof.gatka.webdev@gmail.com>
2023-03-31 14:06:47 +0200

    feat: pantry-element template

--- src/app/pantry/pantry-element/pantry-element.component.ts
+++ src/app/pantry/pantry-element/pantry-element.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Product } from 'src/app/shopping-list/shopping-list-element/product.model';
 
 @Component({
   selector: 'app-pantry-element',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./pantry-element.component.scss']
 })
 export class PantryElementComponent {
+  @Input() product!: Product;
+  editMode: boolean = false;
 
+  onEditClick(): void {
+    this.editMode = true;
+  }
+
+  onSaveClick(): void {
+    this.editMode = false;
+  }
 }
