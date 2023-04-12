f33fe3ded172302aa1fe320f31413f1d12692f90
Krzysztof Gatka <krzysztof.gatka.webdev@gmail.com>
2023-04-12 16:39:33 +0200

    feat: add availability of ings in pantry in recipes list

--- src/app/recipes/recipes-list/recipes-list-element/recipes-list-element.component.ts
+++ src/app/recipes/recipes-list/recipes-list-element/recipes-list-element.component.ts
@@ -1,12 +1,26 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Recipe } from '../../recipe/recipe.model';
+import { PantryService } from 'src/app/pantry/pantry.service';
 
 @Component({
   selector: 'app-recipes-list-element',
   templateUrl: './recipes-list-element.component.html',
   styleUrls: ['./recipes-list-element.component.scss']
 })
-export class RecipesListElementComponent {
+export class RecipesListElementComponent implements OnInit{
   @Input() recipe!: Recipe;
   @Input() recipesType: string = ''
+  ingsInPantry: number = 0;
+
+  constructor(private pantryService: PantryService) {}
+
+  ngOnInit(): void {
+    const pantry = this.pantryService.getPantry();
+    this.recipe.ingredients.map((ingredient) => {
+      const ingInPantry = pantry.find(product => product.name === ingredient.name)
+      if (ingInPantry?.quantity! >= ingredient.quantity) {
+        this.ingsInPantry++;
+      }
+    })
+  }
 }
