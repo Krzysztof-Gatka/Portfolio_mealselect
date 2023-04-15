c368b63f0d0c7f47cbf81b1b1e6b29168b98fca9
Krzysztof Gatka <krzysztof.gatka.webdev@gmail.com>
2023-04-15 13:22:52 +0200

    feat: fetching pantry when user is logged in

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PantryService } from '../pantry/pantry.service';
 
 @Component({
   selector: 'app-recipes',
   templateUrl: './recipes.component.html',
   styleUrls: ['./recipes.component.scss']
 })
-export class RecipesComponent {
+export class RecipesComponent implements OnInit{
 
+  constructor(private pantryService: PantryService) {}
+
+  ngOnInit(): void {
+    this.pantryService.fetchPantry();
+  }
 }
