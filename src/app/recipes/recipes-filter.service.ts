7358ba885455e48ed1ae5515938dc73fb3c9054d
Krzysztof Gatka <krzysztof.gatka.webdev@gmail.com>
2023-05-03 23:51:10 +0200

    feat: add search functionality

--- src/app/recipes/recipes-filter.service.ts
+++ src/app/recipes/recipes-filter.service.ts
@@ -5,74 +5,76 @@ import { RecipesService } from "./recipes.service";
 @Injectable({providedIn: 'root'})
 export class RecipesFilterService {
 
-  constructor(private recipesService: RecipesService) {}
+  // filter(recipesType: string, name: string | null, prepTime: number | null, difficulty: string | null, tags: string[]): Recipe[] {
+  //   let filteredRecipes: Recipe[] = [];
 
-  filter(recipesType: string, name: string | null, prepTime: number | null, difficulty: string | null, tags: string[]): Recipe[] {
-    let filteredRecipes: Recipe[] = [];
+  //   filteredRecipes = this.recipesService.getRecipes(recipesType);
 
-    filteredRecipes = this.recipesService.getRecipes(recipesType);
+  //   if(name !== null) {
+  //     filteredRecipes = filteredRecipes.filter(recipe => {
+  //       return recipe.name.toLowerCase().indexOf(name.toLowerCase()) !== -1;
+  //     })
+  //   }
 
-    if(name !== null) {
-      filteredRecipes = filteredRecipes.filter(recipe => {
-        return recipe.name.toLowerCase().indexOf(name.toLowerCase()) !== -1;
-      })
-    }
+  //   if(prepTime !== null){
+  //     filteredRecipes = filteredRecipes.filter(recipe => {
+  //       return recipe.prepTime <= prepTime;
+  //     })
+  //   }
 
-    if(prepTime !== null){
-      filteredRecipes = filteredRecipes.filter(recipe => {
-        return recipe.prepTime <= prepTime;
-      })
-    }
+  //   if(difficulty !== null) {
+  //     filteredRecipes = filteredRecipes.filter(recipe => {
+  //       return recipe.difficulty.toLowerCase().indexOf(difficulty.toLowerCase()) !== -1;
+  //     })
+  //   }
 
-    if(difficulty !== null) {
-      filteredRecipes = filteredRecipes.filter(recipe => {
-        return recipe.difficulty.toLowerCase().indexOf(difficulty.toLowerCase()) !== -1;
-      })
-    }
+  //   if(tags.length !== 0) {
+  //     filteredRecipes = filteredRecipes.filter(recipe => {
+  //       const recipeTags = recipe.tags;
+  //       if (!recipeTags) return false;
+  //       return recipeTags.some((recipeTag) => tags.some((tag) => tag===recipeTag))
+  //     })
+  //   }
 
-    if(tags.length !== 0) {
-      filteredRecipes = filteredRecipes.filter(recipe => {
-        const recipeTags = recipe.tags;
-        if (!recipeTags) return false;
-        return recipeTags.some((recipeTag) => tags.some((tag) => tag===recipeTag))
-      })
-    }
+  //   return filteredRecipes.slice();
+  // }
 
-    return filteredRecipes.slice();
-  }
+  // sort(recipes: Recipe[], sortingValue: string): Recipe[] {
+  //   const diffSortHelp = {
+  //     easy: 1,
+  //     medium: 2,
+  //     hard: 3,
+  //   }
 
-  sort(recipes: Recipe[], sortingValue: string): Recipe[] {
-    const diffSortHelp = {
-      easy: 1,
-      medium: 2,
-      hard: 3,
-    }
+  //   let sortedRecipes = recipes.slice();
 
-    let sortedRecipes = recipes.slice();
+  //   switch(sortingValue){
+  //     case 'prepTimeAscending':
+  //       sortedRecipes.sort((a, b) => {
+  //         return a.prepTime - b.prepTime;
+  //       });
+  //       break;
+  //     case 'prepTimeDescending':
+  //       sortedRecipes.sort((a, b) => {
+  //         return b.prepTime - a.prepTime;
+  //       });
+  //       break;
+  //     case 'difficultyAscending':
+  //       sortedRecipes.sort((a, b) => {
+  //         return diffSortHelp[a.difficulty as keyof typeof diffSortHelp] - diffSortHelp[b.difficulty as keyof typeof diffSortHelp];
+  //       });
+  //       break;
+  //     case 'difficutlyDescending':
+  //       sortedRecipes.sort((a, b) => {
+  //         return diffSortHelp[b.difficulty as keyof typeof diffSortHelp] - diffSortHelp[a.difficulty as keyof typeof diffSortHelp];
+  //       });
+  //       break;
+  //   }
+  //   sortedRecipes
+  //   return sortedRecipes.slice();
+  // }
 
-    switch(sortingValue){
-      case 'prepTimeAscending':
-        sortedRecipes.sort((a, b) => {
-          return a.prepTime - b.prepTime;
-        });
-        break;
-      case 'prepTimeDescending':
-        sortedRecipes.sort((a, b) => {
-          return b.prepTime - a.prepTime;
-        });
-        break;
-      case 'difficultyAscending':
-        sortedRecipes.sort((a, b) => {
-          return diffSortHelp[a.difficulty as keyof typeof diffSortHelp] - diffSortHelp[b.difficulty as keyof typeof diffSortHelp];
-        });
-        break;
-      case 'difficutlyDescending':
-        sortedRecipes.sort((a, b) => {
-          return diffSortHelp[b.difficulty as keyof typeof diffSortHelp] - diffSortHelp[a.difficulty as keyof typeof diffSortHelp];
-        });
-        break;
-    }
-    sortedRecipes
-    return sortedRecipes.slice();
+  search(searchWords: string[]) {
+
   }
 }
--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -2,7 +2,6 @@ import { Component, Input, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { Recipe } from '../recipe/recipe.model';
-import { RecipesFilterService } from '../recipes-filter.service';
 import { RecipesService } from '../recipes.service';
 
 export const Recipes_Base = 'recipes-base';
@@ -16,26 +15,15 @@ export const Community_Recipes = 'community-recipes';
 })
 export class RecipesListComponent implements OnInit{
   recipes: Recipe[] | undefined;
-  tags: string[] = [];
 
   recipesType: string = '';
-  sorting: boolean = false;
-  filtering: boolean = false;
   loading: boolean = true;
   filterSortMenuOpened: boolean = false;
-  sortingv2: {type: string, asc: boolean} = {type: 'none', asc: false};
+  sorting: {type: string, asc: boolean} = {type: 'none', asc: false};
 
-  filterForm = new FormGroup({
-    name: new FormControl(''),
-    prepTime: new FormControl(null),
-    difficulty: new FormControl(''),
-    price: new FormControl(''),
-    tags: new FormControl('', Validators.required),
-  });
-
-  sortForm = new FormGroup({
-    sort: new FormControl(''),
-  });
+  searchForm = new FormGroup({
+    search: new FormControl(''),
+  })
 
   sliderForm = new FormGroup({
     time: new FormControl(''),
@@ -43,7 +31,6 @@ export class RecipesListComponent implements OnInit{
 
   constructor (
     private recipesService: RecipesService,
-    private recipesFilterService: RecipesFilterService,
     private route: ActivatedRoute
   ) {}
 
@@ -62,60 +49,22 @@ export class RecipesListComponent implements OnInit{
       } else {
         this.recipesService.fetchRecipes(this.recipesType);
       }
-      this.filtering = false;
-      this.sorting = false;
-      this.tags = [];
-      this.sortForm.reset();
-      this.filterForm.reset();
-
     });
+
+    this.searchForm.controls.search.valueChanges.subscribe((input) => {
+      const searchWords = input?.split(" ").filter((word) => word !== '');
+
+      if (searchWords && searchWords.length > 0) {
+        this.loading = true;
+        this.recipes = this.recipesService.filterSearch(searchWords, this.recipesType);
+        console.log(this.recipes.length);
+        this.loading = false;
+      } else {
+        this.recipesService.fetchRecipes(this.recipesType);
+      }
+    })
   }
 
-  onFilterClick(): void {
-    const name = this.filterForm.controls.name.value;
-    const prepTime = this.filterForm.controls.prepTime.value;
-    const difficulty = this.filterForm.controls.difficulty.value;
-
-    this.recipes = this.recipesFilterService.filter(this.recipesType, name, prepTime, difficulty, this.tags);
-    this.filtering = true;
-  }
-
-  onClearFiltersClick(): void {
-    this.filterForm.reset();
-    this.tags = [];
-    this.onFilterClick();
-    this.filtering = false;
-  }
-
-  onSortClick(): void {
-    this.recipes = this.recipesFilterService.sort(this.recipes!, this.sortForm.controls.sort.value!);
-    this.sorting = true;
-  }
-
-  onClearSortClick(): void {
-    this.sortForm.reset();
-    if(this.filtering) {
-      this.onFilterClick();
-    } else {
-      this.recipes = this.recipesService.getRecipes(this.recipesType);
-    }
-    this.sorting = false;
-  }
-
-  onAddTagClick(): void {
-    if(this.tags) {
-      this.tags.push(this.filterForm.controls.tags.value!)
-    } else {
-      this.tags = [this.filterForm.controls.tags.value!];
-    }
-    this.filterForm.controls.tags.reset();
-    this.onFilterClick();
-  }
-
-  onXClick(index: number):void {
-    this.tags = this.tags!.filter((_, i) => index !== i);
-    this.onFilterClick();
-  }
 
   onFilterButtonClick() {
     this.filterSortMenuOpened = !this.filterSortMenuOpened;
@@ -125,16 +74,17 @@ export class RecipesListComponent implements OnInit{
     this.filterSortMenuOpened = false;
   }
 
-  onApplyClick(): void {
-    this.filterSortMenuOpened = false;
-  }
 
   onSortByBtnClick(type: string): void {
-    if(this.sortingv2.type === type) {
-      this.sortingv2.asc = !this.sortingv2.asc;
+    if(this.sorting.type === type) {
+      this.sorting.asc = !this.sorting.asc;
     } else {
-      this.sortingv2.type = type;
-      this.sortingv2.asc = true;
+      this.sorting.type = type;
+      this.sorting.asc = true;
     }
+  }
+
+  onApplyClick(): void {
+    this.filterSortMenuOpened = false;
   }
 }
